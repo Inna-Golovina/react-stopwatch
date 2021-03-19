c0ceba094c93e2f99a0ca1703b47728f9fb5bf04
Inna Golovina <igigig978@gmail.com>
2021-03-19 05:16:37 +0200

    Updates

---
+++ precache-manifest.83b7bc702a390b09db1cca2e53519a80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de92cfefe1c0c96aad68bde95bd0173c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9e7497102aa22d6d8266",
+    "url": "./static/css/main.16ef3f34.chunk.css"
+  },
+  {
+    "revision": "391b06970245a8f35c93",
+    "url": "./static/js/2.44c919cf.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "./static/js/2.44c919cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e7497102aa22d6d8266",
+    "url": "./static/js/main.327a85f5.chunk.js"
+  },
+  {
+    "revision": "d7a8a97f002c3b51fddc",
+    "url": "./static/js/runtime-main.11fcdd35.js"
+  }
+]);
